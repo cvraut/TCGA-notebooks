76b32066fece68d74d65415f67f1d3a5aae94d34
cvraut <chinmay0raut@gmail.com>
2021-09-03 10:51:32 -0400

    fixed get data script a bit

--- get_TCGA_data.R
+++ get_TCGA_data.R
@@ -1,5 +1,5 @@
 library(TCGAbiolinks)
-library(dpylr)
+library(dplyr)
 library(DT)
 
 # reference: https://rpubs.com/tiagochst/TCGAworkshop
@@ -9,11 +9,22 @@ data_categories = c("DNA methylation")
 data_types = list(c(""))
 
 
-query <- GDCquery(project = cancer_code,
-                  data.category = "DNA methylation")
-
 get_indivs <- function(cancer_code,data_cat,i=1,j=Inf){
   q <- GDCquery(cancer_code,data_cat)
   cases <- q$results[[1]]$cases
   return(cases[i:min(j,length(cases))])
 }
+
+get_meth_data <- function(cancer_code,data_cat,indivs){
+  q <- GDCquery(project = cancer_code,
+                data.category = data_cat,
+                barcode = indivs)
+  GDCdownload(q)
+  data <- GDCprepare(q,summarizedExperiment = TRUE)
+  return(data %>% rowRanges %>% as.data.frame)
+}
+
+tmp_i <- get_indivs("TCGA-BRCA","DNA Methylation",j=200)
+meth <- get_meth_data("TCGA-BRCA","DNA Methylation",tmp_i)
+
+save.image()
